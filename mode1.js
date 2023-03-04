aa20b97668e56b7a159a98899651937407054475
Mayank Verma <104269706+mayankverma0097@users.noreply.github.com>
2023-03-05 00:46:18 +0530

    Add files via upload

--- mode1.js
+++ mode1.js
@@ -11,6 +11,7 @@
  btn1.addEventListener('click',function(){
     // console.log("button1 clicked");
     body[0].style.background="black";
+    
     btn1.style.display="none";
     btn2.style.display="block";
 
@@ -22,5 +23,34 @@
     body[0].style.background="white";
     btn2.style.display="none";
     btn1.style.display="block";
+
+ })
+
+ //responsive menu button
+
+ btn3=document.getElementById("btn3");
+
+ nav=document.getElementsByClassName("nav");
+ 
+ content=document.getElementsByClassName("content");
+ btn4=document.getElementById("btn4");
+
+ 
+ btn3.addEventListener('click',function(){
+   // console.log("menu button clicked");
+   nav[0].style.display="flex";
+   btn3.style.display="none";
+   btn4.style.display="inline-block";
+   content[0].style.margin="5vh 10vw";
+   
+
+ })
+ btn4.addEventListener('click',function(){
+   // console.log("menu cut button clicked");
+   nav[0].style.display="none";
+   btn4.style.display="none";
+   btn3.style.display="flex";
+   content[0].style.margin="15vh 10vw";
+   
 
  })
