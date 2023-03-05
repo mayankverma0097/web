7f0f99fdb26c470ad8f306dfd8a5665f2f096fed
Mayank Verma <104269706+mayankverma0097@users.noreply.github.com>
2023-03-06 00:11:33 +0530

    Add files via upload

--- mode1.js
+++ mode1.js
@@ -7,6 +7,18 @@
  body=document.getElementsByTagName("body");
  console.log(body);
 
+  //getting classes of diffrent contents and programs
+
+copy=document.getElementsByClassName("copy");
+prog=document.getElementsByClassName("prog");
+prog1=document.getElementsByClassName("prog1");
+content=document.getElementsByClassName("content");
+heading1=document.getElementById("heading1");
+heading2=document.getElementById("heading2");
+nava=document.getElementsByClassName("nava");
+
+
+console.log(nava);
 
  btn1.addEventListener('click',function(){
     // console.log("button1 clicked");
@@ -14,8 +26,14 @@
     
     btn1.style.display="none";
     btn2.style.display="block";
-
-
+    copy[0].style.color="rgb(255, 126, 126)";
+    prog1[0].style.color="white";
+    content[0].style.color="white";
+    heading1.style.color="rgb(255, 126, 126)";
+    heading2.style.color="rgb(255, 126, 126)";
+    nava[0].style.color="rgb(255, 126, 126)";
+    nava[1].style.color="rgb(255, 126, 126)";
+    nava[2].style.color="rgb(255, 126, 126)";
  })
 
  btn2.addEventListener('click',function(){
@@ -23,7 +41,9 @@
     body[0].style.background="white";
     btn2.style.display="none";
     btn1.style.display="block";
-
+    copy[0].style.color="rgb(194, 32, 32)";
+    prog1[0].style.color="black";
+    content[0].style.color="rgb(194, 32, 32)";
  })
 
  //responsive menu button
@@ -35,6 +55,8 @@
  content=document.getElementsByClassName("content");
  btn4=document.getElementById("btn4");
 
+
+
  
  btn3.addEventListener('click',function(){
    // console.log("menu button clicked");
@@ -43,6 +65,7 @@
    btn4.style.display="inline-block";
    content[0].style.margin="5vh 10vw";
    content[0].style.margin="5vh 0vh 0vw 10vw"; 
+  
 
  })
  btn4.addEventListener('click',function(){
